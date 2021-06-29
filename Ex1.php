1b3b0a8633cedcb51066b096c2ce2637039be6fc
nicolekamiha <nicoleayumikamiha@gmail.com>
2021-06-29 17:46:50 -0300

    última versão

---
+++ Ex1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Exercicio 1</title>
+    </head>
+    <body>
+
+    <form method = "get" action="multiplosesinal">
+    Numero <input type = "text" name="txtN" value= "0">
+    <input type="submit" value="Calcular" name="cmdEnviar">
+    </form>
+
+        <?php
+            echo "Olá mundo , eu sou um script PHP!";
+        ?>
+
+    </body>
+</html>
